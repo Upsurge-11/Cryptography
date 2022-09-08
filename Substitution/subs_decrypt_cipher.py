0e1c93c512e0a1e0b96c54117b2e2078532577a2
Arghyadeep Mukherjee <arghyadeepbumbum@gmail.com>
2022-09-08 23:20:16 +0530

    Changes to be committed: 	modified:   Substitution/subs_decrypt_cipher.py

--- Substitution/subs_decrypt_cipher.py
+++ Substitution/subs_decrypt_cipher.py
@@ -1,42 +1,51 @@
-def decrypt_substitution(text, key):
-    result = ""
-    alphabet_mapping = {}
-    translation = {}
-    for c in text:
-        char = c.upper()
-        if char in alphabet_mapping:
-            alphabet_mapping[char] += 1
-        else:
-            alphabet_mapping[char] = 0
-    alphabet_mapping = {key: val for key, val in sorted(
-        alphabet_mapping.items(), key=lambda ele: ele[1], reverse=True)}
-    key_list = list(alphabet_mapping.keys())
-    j = 1
-    for i in range(len(key_list)):
-        translation[key_list[i]] = key[j]
-        j += 1
-    for char in text:
-        if char in translation:
-            result += translation[char]
-        else:
-            result += char
+def decrypt_substitution_cipher(text, size):
+    plaintext = [None] * 26
+    key = "ETAOINSHRDLCUMWFGYPBVKJQXZ"
+    freq = [0] * 26
+    freqSorted = [None] * 26
+    used = [0] * 26
 
-    return result
+    for i in range(size):
+        if text[i] != ' ':
+            freq[ord(text[i]) - 65] += 1
 
+    for i in range(26):
+        freqSorted[i] = freq[i]
 
-def remove_spaces(s):
-    return s.replace(" ", "")
+    freqSorted.sort(reverse=True)
+
+    for i in range(26):
+        ch = -1
+        for j in range(26):
+            if freqSorted[i] == freq[j] and used[j] == 0:
+                used[j] = 1
+                ch = j
+                break
+        if ch == -1:
+            break
+        x = ord(key[i]) - 65
+        x = x - ch
+        curr = ""
+        for k in range(size):
+            if text[k] == ' ':
+                curr += " "
+                continue
+            y = ord(text[k]) - 65
+            y += x
+            if y < 0:
+                y += 26
+            if y > 25:
+                y -= 26
+            curr += chr(y + 65)
+        plaintext[i] = curr
+
+    for i in range(26):
+        print(str(i+1) + ") " + plaintext[i])
 
 
 def main():
-    encrypted_text = input("Enter the encrypted text:- ")
-    encrypted_text = encrypted_text.upper()
-    encrypted_text = remove_spaces(encrypted_text)
-    alphabets = {
-        1: 'E', 2: 'T', 3: 'A', 4: 'O', 5: 'I', 6: 'N', 7: 'S', 8: 'H', 9: 'R', 10: 'D', 11: 'L', 12: 'C', 13: 'U', 14: 'M', 15: 'W', 16: 'F', 17: 'G', 18: 'Y', 19: 'P', 20: 'B', 21: 'V', 22: 'K', 23: 'J', 24: 'Q', 25: 'X', 26: 'Z'
-    }
-    decrypted_text = decrypt_substitution(encrypted_text, alphabets)
-    print("Decrypted text:- " + decrypted_text)
+    encrypted_text = input("Enter the string :- ")
+    decrypt_substitution_cipher(encrypted_text.upper(), len(encrypted_text))
 
 
 if __name__ == "__main__":
